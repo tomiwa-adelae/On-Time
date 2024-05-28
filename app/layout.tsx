cd66e418bcaff9b9a6d30f5d8f02e20f557acad2
Tomiwa Adelae <thetommedia@gmail.com>
2024-05-28 21:50:33 +0100

    Updated the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./provider";
+import Footer from "../components/Footer";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -25,7 +26,8 @@ export default function RootLayout({
 					<div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative">
 						<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_90%,black)]"></div>
 						<Header />
-						{children}
+						<div className="min-h-[80vh]">{children}</div>
+						<Footer />
 						<Toaster />
 					</div>
 				</Providers>
---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="text-sm p-8 text-center">
+			&copy; 2024 On Time. All Rights Reserved.
+		</footer>
+	);
+};
+
+export default Footer;
